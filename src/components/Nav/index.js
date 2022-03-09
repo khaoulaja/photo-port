0a23499f950b4fbacb5109041eb41a27c211897a
khaoula <khaoulajabour4@gmail.com>
2022-03-09 05:30:54 -0500

    deploy application

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -20,7 +20,7 @@ function Nav (props){
     return (
         <header className="flex-row px-1">
           <h2>
-            <a data-testid="link" href="/">
+            <a data-testid="link" href="/photo-port">
               <span role="img" aria-label="camera"> 📸</span> Oh Snap!
             </a>
           </h2>
